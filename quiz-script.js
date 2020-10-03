8909f1b2d746376f2b21b6ec783158861dc5b708
slmn9653 <salman9915189734@gmail.com>
2020-10-03 19:54:21 +0530

    initial files

---
+++ quiz-script.js
@@ -0,0 +1,53 @@
+var currentQuestion = 0;
+var score = 0;
+var totQuestions = questions.length;
+
+var container = document.getElementById('quizContainer');
+var questionEl = document.getElementById('question');
+var opt1 = document.getElementById('opt1');
+var opt2 = document.getElementById('opt2');
+var opt3 = document.getElementById('opt3');
+var opt4 = document.getElementById('opt4');
+var nextButton = document.getElementById('nextButton');
+var resultCont = document.getElementById('result');
+
+function loadQuestion (questionIndex) {
+	var q = questions[questionIndex];
+	questionEl.textContent = (questionIndex + 1) + '. ' + q.question;
+	opt1.textContent = q.option1;
+	opt2.textContent = q.option2;
+	opt3.textContent = q.option3;
+	opt4.textContent = q.option4;
+};
+
+function skipQuestion () {
+	selectedOption = 0;
+	loadNext();
+}
+
+function loadNextQuestion () {
+	selectedOption = document.querySelector('input[type=radio]:checked');
+	if(!selectedOption){
+		alert('Please select your answer !');
+		return;
+	}
+	score+=10;
+	loadNext();
+}
+function loadNext(){
+	answer = selectedOption.value;
+	selectedOption.checked = false;
+	currentQuestion++;
+	if(currentQuestion == totQuestions - 1){
+		skipButton.textContent = 'Finish';
+		nextButton.textContent = 'Finish';
+	}
+	if(currentQuestion == totQuestions){
+		container.style.display = 'none';
+		resultCont.style.display = '';
+		resultCont.textContent = 'Your Score: ' + score + " / 100";
+		return;
+	}
+	loadQuestion(currentQuestion);
+}
+loadQuestion(currentQuestion);
